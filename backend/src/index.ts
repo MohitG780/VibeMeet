c165f26b395a274e951594572a3bd73a28d3b010
MohitG780 <mohitgupta000780@gmail.com>
2025-02-08 23:14:55 +0530

    peertopeerconnection made

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,20 @@
+import { Socket } from "socket.io";
+const { Server }= require('socket.io');
 
+import http from "http";
+const express = require('express');
+
+const { join } = require('node:path');
+const app=express();
+const server=http.createServer(http);
+
+const io=new Server(server);
+
+
+io.on('connection',(socket:Socket)=>{
+console.log('a user connected');
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
