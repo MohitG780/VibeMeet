ef3aeded00babc68b11400868c3c4dc8571f36b7
MohitG780 <mohitgupta000780@gmail.com>
2025-02-09 16:26:44 +0530

    socket done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,27 @@
 import { Socket } from "socket.io";
-const { Server }= require('socket.io');
+import { Server } from 'socket.io';
 
 import http from "http";
-const express = require('express');
+import express from 'express';
+import { UserManager } from "./managers/UserManager";
 
 const { join } = require('node:path');
 const app=express();
 const server=http.createServer(http);
 
-const io=new Server(server);
-
+const io=new Server(server,{
+    cors:{
+        origin:"*"
+    }
+});
+ const userManager=new UserManager();
 
 io.on('connection',(socket:Socket)=>{
 console.log('a user connected');
+userManager.addUser("randomname",socket);
+socket.on("disconnect",()=>{
+  userManager.removeUser(socket.id);
+})
 });
 
 server.listen(3000, () => {
